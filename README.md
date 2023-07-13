2254dccfdbd9f9f2378bb8ebf15f0f1207629450
Zach Young <zacharysyoung@gmail.com>
2023-07-12 17:30:19 -0700

    working

--- README.md
+++ README.md
@@ -7,3 +7,29 @@
 - From "Header canonicalization", HTTP/1 allows for bumpy-case header keys, and Go wants to follow this convention (CanonicalMIMEHeaderKey()).  To get past this update the header map directly (e.g., `w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}`, otherwise Go would convert to X-Xss-Protection).
 
     HTTP/2 requires the server respond will all lowercase, and Go enforces this regardless of direct manipulation.
+
+- Handlers satisfy the http.Handler interface, which has the sole method ServeHTTP(ResponseWriter, *Request).
+
+    ```go
+    type home struct {}
+
+    func (h *home) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+        ...
+    }
+
+    ...
+
+    mux := http.NewServerMux()
+    mux.Handle("/", &home{})
+    ```
+
+    is equivalent to what we currently have:
+
+    ```go
+    mux := http.NewServerMux()
+    mux.HandleFunc("/", home)
+    ```
+
+    where home is an actual Handler function—internally, mux.HandleFunc just wraps home, like HandlerFunc(home), to generate something (?) with the ServeHTTP method.
+
+## Ch 3
--- cmd/web/SnippetBox/handlers.go
+++ cmd/web/SnippetBox/handlers.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 	"text/template"
 )
 
 // home returns home.
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -23,20 +22,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
+		app.errorLog.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
+		app.errorLog.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
 }
 
 // snippetView returns a specific snippet.
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -47,7 +46,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 // snippetCreate creates a new snippet.
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
