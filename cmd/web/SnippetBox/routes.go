a8ef2610bf42a87f0f23951b2d882a27fda8af30
Zach Young <zacharysyoung@gmail.com>
2023-07-19 12:30:10 -0700

    added alice for middleware management

--- cmd/web/SnippetBox/routes.go
+++ cmd/web/SnippetBox/routes.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
@@ -14,5 +16,7 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/zacharysyoung/SnippetBox
 
 go 1.20
 
-require github.com/go-sql-driver/mysql v1.7.1
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/justinas/alice v1.2.0
+)
