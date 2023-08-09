bd23c83373d71a573cc91295176cde1065a55a4b
Zach Young <zacharysyoung@gmail.com>
2023-08-08 18:57:56 -0700

    added logout

--- cmd/web/SnippetBox/handlers.go
+++ cmd/web/SnippetBox/handlers.go
@@ -208,11 +208,21 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.sessionManager.Put(r.Context(), "authenticateUserID", id)
+	app.sessionManager.Put(r.Context(), "authenticatedUserID", id)
 
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "You've been successfully logged out!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
